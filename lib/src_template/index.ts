877980c384ff55dcf7d6031552bf4009da0ad9f7
Mayank <mayank.srmu@gmail.com>
2025-03-25 12:53:36 +0530

    Update template
    - Will fine tune after fixing the @m2d/core package

--- lib/src_template/index.ts
+++ lib/src_template/index.ts
@@ -1,13 +1,10 @@
 import { IPlugin } from "@m2d/core";
-import emojis from "./emoji.json";
 
 export const emojiPlugin: () => IPlugin = () => ({
   inline: async (_docx, node) => {
-    if (node.type === "text") {
-      node.value = node.value.replace(
-        /:[a-z0-9_+-]+:/g,
-        (match: keyof typeof emojis) => emojis[match] ?? match,
-      );
+    if (node.type === "supported_type") {
+      /** process node and return array of  */
+      node.type = "";
     }
     return [];
   },
